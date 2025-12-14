a69ce8240fd2e4549715cfc653655a397f1734df
Waymartcorp <60561446+Waymartcorp@users.noreply.github.com>
2025-12-14 12:20:34 -0500

    Delete src/api/thread directory

--- src/api/thread/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { ensureThread, db } from "@/lib/db";
-
-export async function POST() {
-  const threadId = ensureThread();
-  const thread = db.threads.get(threadId)!;
-  return NextResponse.json(thread);
-}
