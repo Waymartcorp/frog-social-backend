dc6e556caf3d4dcd63b1d38c9d0819b7ecda7401
Waymartcorp <60561446+Waymartcorp@users.noreply.github.com>
2025-12-28 01:35:04 -0500

    Create protocols.ts

---
+++ lib/protocols.ts
@@ -0,0 +1,9 @@
+export const ROBUFFER_RECIPE = {
+  name: "ROBUFFER™",
+  baseVolumeLiters: 100,
+  ingredients: [
+    { name: "Calcium Chloride Dihydrate", amount: 5.0, unit: "g" },
+    { name: "Epsom Salt", amount: 2.5, unit: "g" },
+    { name: "Baking Soda", amount: 3.5, unit: "g" }
+  ]
+};
